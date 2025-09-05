3344a2d15a433a5b769fb55e3ed1329e268513cc
derekpierre <derek.pierre@gmail.com>
2025-09-05 19:37:22 -0400

    Update run.sh to also run renderer script. Update env variable template to make variables more specific and applicable to proof-bot and renderer where appropriate.
    Co-authored-by: Viktoriia Zotova <vikki.zta@gmail.com>

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,8 +3,14 @@
 cd /app
 echo "The train is leaving the station"
 ape run proof_bot \
---network $NETWORK          \
+--network $ETH_NETWORK      \
 --account $ACCOUNT          \
 --fx-root-tunnel $TUNNEL    \
 --graphql-endpoint $GQL_URL \
 --proof-generator $PROOFS_URL
+
+ape run resender \
+--network $POLYGON_NETWORK \
+--taco-child-application $TACO_CHILD_APPLICATION_ADDRESS \
+--account $ACCOUNT \
+--graphql-endpoint $GQL_URL
